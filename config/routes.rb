97379268cd4a4d6732179515c54ebb5c2accac06
end3rs-army <eric.m.dowty@gmail.com>
2015-08-10 10:51:58 -0600

    wired up model, route, controller, and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  def index
-    @ideas = Idea.all
-  end
+  resources :ideas, only: [:index]
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+class Seed
+
+  def self.go
+    create_ideas
+  end
+
+  def self.create_ideas
+    10.times { Idea.create(title: Faker::Lorem.word, body: Faker::Lorem.sentence) }
+  end
+
+end
+
+Seed.go
